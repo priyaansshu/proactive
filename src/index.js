d75622c1ed8a1b27e511787a69c36269c6ba1045
priyaansshu <81822167+priyaansshu@users.noreply.github.com>
2022-01-27 19:33:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+<HashRouter>
   <App />
-</BrowserRouter>,
+</HashRouter>,
   document.getElementById('root') //this line is telling the react app to inject the line 9 content to this selected element.
 );
 reportWebVitals();
